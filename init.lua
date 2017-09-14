f7f796c2d31b27c27b3d7f11810bc90502e650a5
Aleksander Petrovskii <petr0vskjy.aleksander@gmail.com>
2017-09-14 16:14:09 +0300

    Connect to cloud mqtt broker

---
+++ init.lua
@@ -0,0 +1,35 @@
+--------------------------------
+-- init file for sensor base on SHT1x chip
+-----------------------------------
+
+-- load SID and Password
+dofile("credential.lua")
+-- if any thing done wrong - delete init.lua
+function startup()
+  if file.open("init.lua") == nil then
+    print("-- init.lua deleted --")
+  else
+    print("-- Running --")
+    file.close("init.lua")
+  end
+end
+----init esp8266 --------
+print("-- Set up wi-fi mode --")
+wifi.setmode(wifi.STATION)
+wifi.sta.config(SSID, PASSWORD)
+wifi.sta.autoconnect(1)
+-- check connect
+tmr.alarm(1,1000,tmr.ALARM_AUTO, function()
+  if wifi.sta.getip() == nil then
+    print("-- IP unavailable, Waiting... --")
+  else
+    tmr.stop(1)
+    print("ESP8266  mode is: " .. wifi.getmode())
+    print("MAC address is: ".. wifi.ap.getmac())
+    print("IP  address is: " .. wifi.sta.getip())
+    print("-- You have 5 second to abort Startup --")
+    print("-- Waiting --")
+    --wifi.sta.getap(listap)
+    dofile("mqtt.lua")
+  end
+end)
---
+++ mqtt.lua
@@ -0,0 +1,41 @@
+----------------------------------------
+--- mqtt client for SHT1x sensor
+-----------------------------------------
+
+-- connect to mqtt broker at    https://www.cloudmqtt.com/
+m = mqtt.Client("clientid"..node.chipid(), 120, "ucnivmtq", "7EFWERh6IGGs")
+m:on("connect", function(client) print ("connected") end)
+m:on("offline", function(client) print ("offline") end)
+print("mqtt.lua")
+-- connect to cloud broker and resive temp and hum --
+m:connect("m20.cloudmqtt.com", 11448, 0, function(client)
+  print("connected")
+-- subscribe topic with qos = 0
+  client:subscribe("test", 0, function(client) print("subscribe success") end)
+  -- publish a message with data = hello, QoS = 0, retain = 0
+  client:publish("test", "hello from ESP82661 SHT1x sensor", 0, 0, function(client) print("sent") end)
+end,
+
+function(client, reason)
+  print("failed reason: " .. reason)
+end)
+
+function publish_data()
+
+  m:publish("test",t, 0,0, function(conn)
+    --- get SHT1x data and print to cloud broker
+    data = dofile("sht1x_v2.lua")
+    temp = (data[1])
+    hum  = (data[2])
+    print("temp: "..temp, "humm: "..hum)
+  	data = nil
+  	temp = nil
+  	hum = nil
+  	btemp = nil
+  	collectgarbage()
+
+  end)
+end
+
+--
+tmr.alarm(2, 6000, 1, publish_data)
